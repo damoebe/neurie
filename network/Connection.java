5e65d354063e770bb96f0e90e7fa06aff45df2f9
damoebe <100862528+damoebe@users.noreply.github.com>
2025-04-16 15:49:43 +0200

    Neuron activation-generator
    simple setup of Neuron and Connection class with a function to update the neurons activation.

--- network/Connection.java
+++ network/Connection.java
@@ -1,4 +1,19 @@
 package network;
 
 public class Connection {
+    private double weight;
+    private Neuron targetNeuron;
+
+    public Connection(Neuron targetNeuron) {
+        this.weight = 1; // initial weight for network
+        this.targetNeuron = targetNeuron;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public Neuron getTargetNeuron() {
+        return targetNeuron;
+    }
 }
--- network/Neuron.java
+++ network/Neuron.java
@@ -1,4 +1,34 @@
 package network;
 
+import java.util.List;
+
 public class Neuron {
+
+    private final List<Connection> connections; // List of all dependencies
+    private double activation = 0; // the neurons current activation
+
+
+    public Neuron(List<Connection> connections){
+        this.connections = connections;
+    }
+
+    // updates the neurons current activation based on the connections
+    public void updateActivation(){
+        double newActivation = 0;
+        for (Connection connection : connections){
+            newActivation += connection.getWeight() * connection.getTargetNeuron().getActivation();
+        }
+        if (!connections.isEmpty()) { // if not first layer neuron
+            activation = newActivation / connections.size();
+        }
+    }
+
+    public double getActivation(){
+        return activation;
+    }
+
+    // for input neurons
+    public void setActivation(double activation){
+        this.activation = activation;
+    }
 }
